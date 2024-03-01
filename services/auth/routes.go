0df3f15ede09329e8589c27c87174d15dda12f09
Philix27 <philixbob@gmail.com>
2024-03-01 13:48:48 +0100

    chore[auth]: added routes

--- services/auth/routes.go
+++ services/auth/routes.go
@@ -5,9 +5,33 @@ import (
 )
 
 func RoutesHandler(router fiber.Router) {
-	router.Get("/", announce)
-	// return c.SendStatus(fiber.StatusOK)
+	router.Post("/create_user", createUser)
+	router.Post("/send_email_otp", sendEmailOtp)
+	router.Post("/verify_email_otp", verifyEmailOtp)
+	router.Post("/login", login)
+	router.Post("/resent_otp", resendOtp)
+	router.Post("/logout", logout)
+	router.Get("/get_auth_credentials", getAuthCredentials)
 }
-func announce(c *fiber.Ctx) error {
+
+func sendEmailOtp(c *fiber.Ctx) error {
+	return c.SendString("Borrow Checker")
+}
+func verifyEmailOtp(c *fiber.Ctx) error {
+	return c.SendString("Borrow Checker")
+}
+func createUser(c *fiber.Ctx) error {
+	return c.SendString("Borrow Checker")
+}
+func login(c *fiber.Ctx) error {
+	return c.SendString("Borrow Checker")
+}
+func resendOtp(c *fiber.Ctx) error {
+	return c.SendString("Borrow Checker")
+}
+func logout(c *fiber.Ctx) error {
+	return c.SendString("Borrow Checker")
+}
+func getAuthCredentials(c *fiber.Ctx) error {
 	return c.SendString("Borrow Checker")
 }
