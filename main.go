70ffb6b142dd1520d743383443993a9a6fff7648
Philix27 <philixbob@gmail.com>
2024-03-03 23:09:14 +0100

    added swagger docs generator

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	_ "harmony/docs"
 	"harmony/middleware"
 	"harmony/services/announcement"
 	"harmony/services/auth"
@@ -15,7 +15,19 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/swagger"
 )
+
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 
 func main() {
 	app := fiber.New(fiber.Config{
@@ -44,14 +56,32 @@ func main() {
 	v1.Route("/user", user.RoutesHandler)
 	v1.Route("/wiki", wiki.RoutesHandler)
 
-	fmt.Println("Handlers Count")
+	// fmt.Println("Handlers Count")
 
-	for i, data := range app.GetRoutes() {
-		fmt.Println("Index: %v Name:", i+1, data.Name)
-		fmt.Println("Index: %v Params:", i+1, data.Params)
-		fmt.Println("Index: %v Handlers:", i+1, data.Handlers)
-		fmt.Println("Index: %v Path:", i+1, data.Path)
-	}
+	// for i, data := range app.GetRoutes() {
+	// 	fmt.Println("Index: %v Name:", i+1, data.Name)
+	// 	fmt.Println("Index: %v Params:", i+1, data.Params)
+	// 	fmt.Println("Index: %v Handlers:", i+1, data.Handlers)
+	// 	fmt.Println("Index: %v Path:", i+1, data.Path)
+	// }
+
+
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
+
+	// app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+	// 	URL: "http://example.com/doc.json",
+	// 	DeepLinking: false,
+	// 	// Expand ("list") or Collapse ("none") tag groups by default
+	// 	DocExpansion: "none",
+	// 	// Prefill OAuth ClientId on Authorize popup
+	// 	OAuth: &swagger.OAuthConfig{
+	// 		AppName:  "OAuth Provider",
+	// 		ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
+	// 	},
+	// 	// Ability to change OAuth2 redirect uri location
+	// 	OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
+	// }))
+
 
 	log.Fatal(app.Listen(":3111"))
 }
