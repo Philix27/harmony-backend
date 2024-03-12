b46ec24672487390531e6326a711c0e753a54e14
Philix27 <philixbob@gmail.com>
2024-03-12 12:46:45 +0100

    chore: add a makefile

---
+++ _social/if.go
@@ -0,0 +1,21 @@
+// Go - If statement
+
+var y = 3
+
+// brackets wrapping the conditionals 
+// are not needed unlike other languages
+if y > 2 {
+    // code to execute if condition is true
+} else {
+    // code to execute if condition is false
+}
+
+// This is called an "initializer statement"
+// x is scoped to the if statement.
+if x := someFunction(); x < 0 {
+    // code to execute if x is less than 0
+} else if x == 0 {
+    // code to execute if x is equal to 0
+} else {
+    // code to execute if x is greater than 0
+}
